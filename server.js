54a5b16b2eeff210eba3b27e67a79a9329da5bf1
Gustavo Caris <gustavocaris14052002@gmail.com>
2025-08-29 15:44:54 -0300

    Tentando resolver a rota de comunicação com o banco e addicionando mais duas functions no serverJS relacionadas as req GET e PUT

--- server.js
+++ server.js
@@ -21,14 +21,64 @@ app.post('/usuarios', async (req, res) => {
         }
     })
 
-    //users.push(req.body)
+    users.push(req.body)
 
     res.status(201).json(req.body);
 });
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+
+    if(req.query) {
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age
+            }
+        })
+        
+    } else {
+        users = await prisma.user.findMany()
+    }
+
+
+    const users = await prisma.user.findMany()
+
+
     res.status(200).json(users)
 });
+
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            // caminho de onde estou recebendo essas minhas infos
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    users.push(req.body)
+
+    res.status(201).json(req.body);
+});
+
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({ message: "Usuario Deletado com Sucesso"})
+})
+
+
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
